d88f308b35e435d7589b461df8c1abe6f58163c3
alif <aliftareq@gmail.com>
2023-06-21 10:24:38 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-const port = 3000
 
 //
 app.use(cors())
@@ -10,11 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 //testing api's
-app.get('/', (req: Request, res: Response) => {
-    res.send('Working Successfully')
+app.get('/', (req: Request, res: any) => {
+  res.send('Working Successfully')
 })
-
 
 export default app
